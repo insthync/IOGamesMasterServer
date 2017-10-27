ab0268d9285b5d68852c4f13442ca358bcae79d0
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-10-27 23:11:18 +0700

    Master server WIP

---
+++ Scripts/UINetworkRoom.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Barebones.MasterServer;
+using Barebones.Networking;
+
+public class UINetworkRoom : UIBase
+{
+    public UINetworkRoomEntry entryPrefab;
+    public Transform gameListContainer;
+    private readonly List<UINetworkRoomEntry> entries = new List<UINetworkRoomEntry>();
+
+    protected IClientSocket Connection = Msf.Connection;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        ClearEntries();
+        Connection.Connected += OnConnectedToMaster;
+    }
+
+    void OnDestroy()
+    {
+        Msf.Connection.Connected -= OnConnectedToMaster;
+    }
+
+    void OnEnable()
+    {
+        if (Connection.IsConnected)
+            RequestRooms();
+    }
+
+    void OnConnectedToMaster()
+    {
+        // Get rooms, if at the time of connecting the lobby is visible
+        if (gameObject.activeSelf)
+            RequestRooms();
+    }
+
+    void ClearEntries()
+    {
+        for (var i = gameListContainer.childCount - 1; i >= 0; --i)
+        {
+            var child = gameListContainer.GetChild(i);
+            Destroy(child.gameObject);
+        }
+        entries.Clear();
+    }
+
+    void RequestRooms()
+    {
+        if (!Connection.IsConnected)
+        {
+            Logs.Error("Tried to request rooms, but no connection was set");
+            return;
+        }
+
+        var loadingPromise = Msf.Events.FireWithPromise(Msf.EventNames.ShowLoading, "Retrieving Rooms list...");
+
+        Msf.Client.Matchmaker.FindGames(games =>
+        {
+            loadingPromise.Finish();
+            ClearEntries();
+            foreach (var game in games)
+            {
+                var newEntry = Instantiate(entryPrefab, gameListContainer);
+                newEntry.SetData(game);
+                newEntry.gameObject.SetActive(true);
+                entries.Add(newEntry);
+            }
+        });
+    }
+
+    public void OnClickRefresh()
+    {
+        RequestRooms();
+    }
+
+    public override void Show()
+    {
+        base.Show();
+        OnClickRefresh();
+    }
+}
---
+++ Scripts/UINetworkRoomEntry.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Barebones.MasterServer;
+
+public class UINetworkRoomEntry : MonoBehaviour
+{
+    public Text textRoomName;
+    public Text textSceneName;
+    public Text textPlayerCount;
+    public string defaultMapName = "";
+    private GameInfoPacket _data;
+    public void SetData(GameInfoPacket data)
+    {
+        _data = data;
+        if (textRoomName != null)
+            textRoomName.text = data.Name;
+        if (textSceneName != null)
+        {
+            textSceneName.text = data.Properties.ContainsKey(MsfDictKeys.MapName)
+                ? data.Properties[MsfDictKeys.MapName] : defaultMapName;
+        }
+        if (textPlayerCount != null)
+        {
+            if (data.MaxPlayers > 0)
+                textPlayerCount.text = data.OnlinePlayers + "/" + data.MaxPlayers;
+            else
+                textPlayerCount.text = data.OnlinePlayers.ToString();
+        }
+    }
+
+    public void OnClickJoinButton()
+    {
+        Msf.Client.Rooms.GetAccess(_data.Id, OnPassReceived);
+    }
+
+    protected virtual void OnPassReceived(RoomAccessPacket packet, string errorMessage)
+    {
+        if (packet == null)
+        {
+            Msf.Events.Fire(Msf.EventNames.ShowDialogBox, DialogBoxData.CreateError(errorMessage));
+            Logs.Error(errorMessage);
+            return;
+        }
+
+        // Hope something handles the event
+    }
+}
